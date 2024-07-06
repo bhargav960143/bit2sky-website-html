e5cfcd0a8d4ff9eb3fe6b0454a5a37994500ea5d
Krunalsinh <krunal5281@gmail.com>
2024-07-06 10:26:50 +0530

    home page changes

--- js/custom_new.js
+++ js/custom_new.js
@@ -14,14 +14,21 @@ $(document).ready(function() {
     $("#clientLogoSlider").owlCarousel({
         items: 5,
         loop: true,
-        // autoplay: true,
-        margin: 20,
+        autoplay: true,
+        margin: 0,
         nav: false,
         dots: false,
         autoplayTimeout: 3500,
         autoplayHoverPause: true,
         smartSpeed: 2000,
-       
+        responsive : {
+            0:{items:1},
+            520:{items:2},
+            768:{items:3},
+            1200:{items:3},
+            1366:{items:4},
+            1600:{items:5}
+        }
     }); 
     
 })
