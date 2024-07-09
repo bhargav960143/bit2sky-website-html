87497194e5562be17fded968ace03b12a62f2517
Bhargav <bhargav960143@gmail.com>
2024-07-09 13:46:56 +0530

    updated services page

--- js/custom_new.js
+++ js/custom_new.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
     $("#bannerSlider").owlCarousel({
         items: 1,
-        loop: true,
+        loop: false,
         // autoplay: true,
         margin: 20,
-        nav: true,
+        nav: false,
         dots: false,
         autoplayTimeout: 3500,
-        autoplayHoverPause: true,
+        autoplayHoverPause: false,
         smartSpeed: 2000,
-       
+        mouseDrag: false,
+        touchDrag: false,
     }); 
 })
